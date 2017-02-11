ff3f70088454976cbb913c4781025320a28b2f2f
José L. Redrejo Rodríguez <jredrejo@debian.org>
2017-02-11 20:04:44 +0100

    finished clon of the example, with react working

--- static/src/testindex.js
+++ static/src/testindex.js
@@ -1,18 +1,21 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
 
-const Leaderboard = ({users}) =>
-    <div>
-        <h1>Featured Players</h1>
 
-        {users.map(user =>
-            <a href={`/user/${user.username}/`}>
-                {user.username}
-            </a>)}
-    </div>
 
+const Leaderboard = function({users}) {
+          return (
+                <div>
+                    <h1>Featured Players</h1>
+                    <ul>
+                    {users.map(user =>
+                        <li><a href={`/user/${user.username}/`}>
+                            {user.username}
+                        </a></li>)}
+                     </ul>
+                </div>
+          )
+        }
 
 ReactDOM.render(
     React.createElement(Leaderboard, window.props),    // gets the props that are passed in the template
-    window.react_mount,                                // a reference to the #react div that we render to
+    window.react_mount                                // a reference to the #react div that we render to
 )

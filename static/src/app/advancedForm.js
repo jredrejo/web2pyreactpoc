66b16cd42af3f7ffc1692846e81b87cd5a932367
José L. Redrejo Rodríguez <jredrejo@debian.org>
2017-05-28 17:17:14 +0200

    missing files for the redux-form stuff

---
+++ static/src/app/advancedForm.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SimpleForm from './SimpleForm'
+import { Provider } from 'react-redux'
+import store from './store'
+
+class PageForm extends React.Component {
+  submit (values) {
+    console.log(values)
+  }
+  render () {
+    return (
+      <SimpleForm onSubmit={this.submit} />
+    )
+  }
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Simple Form</h2>
+      <PageForm />
+    </div>
+  </Provider>,
+  window.react_mount
+)
